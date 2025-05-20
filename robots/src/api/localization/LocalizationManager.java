5d8e54815dccc060e164e60f08296cd3159d566e
Denis-Mukhametshin-74 <Muhametshin-Den.05@yandex.ru>
2025-05-20 17:49:20 +0500

    feat: Added basic classes for localization

--- robots/src/api/localization/LocalizationManager.java
+++ robots/src/api/localization/LocalizationManager.java
@@ -1,12 +1,85 @@
 package api.localization;
 
-import log.Logger;
+import java.text.MessageFormat;
 
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
 import javax.swing.UIManager;
+
+import log.Logger;
 
 public final class LocalizationManager
 {
+    private static ResourceBundle bundle;
+    private static Locale currentLocale;
+    private static final Map<String, MessageFormat> messageFormatCache = new HashMap<>();
+
+    public static void setLocale(Locale locale)
+    {
+        try
+        {
+            currentLocale = locale;
+            Locale.setDefault(locale);
+            bundle = ResourceBundle.getBundle("localization.messages", locale);
+
+            UIManagerConfigurator.configureUIManager();
+
+            Logger.debug("Locale set to: " + locale);
+        }
+        catch (MissingResourceException e)
+        {
+            Logger.error("Resource bundle not found for locale: " + locale);
+            bundle = ResourceBundle.getBundle("localization.messages", SupportedLocale.RUSSIAN.getLocale());
+        }
+    }
+
+    public static Locale getCurrentLocale()
+    {
+        return currentLocale;
+    }
+
+    public static String getString(String key)
+    {
+        try
+        {
+            return bundle.getString(key);
+        }
+        catch (MissingResourceException e)
+        {
+            Logger.error("Missing resource for key: " + key);
+            return "!" + key + "!";
+        }
+    }
+
+    public static String getFormattedString(String key, Object... args)
+    {
+        String pattern = getString(key);
+        return MessageFormat.format(pattern, args);
+    }
+
+    public static String getCachedFormattedString(String key, Object... args)
+    {
+        String pattern = getString(key);
+
+        MessageFormat format = messageFormatCache.get(pattern);
+        if (format == null)
+        {
+            format = new MessageFormat(pattern, currentLocale);
+            messageFormatCache.put(pattern, format);
+        }
+
+        return format.format(args);
+    }
+
+    public static SupportedLocale[] getSupportedLocales()
+    {
+        return SupportedLocale.values();
+    }
+
     public static void setRussianLocale()
     {
         try
@@ -33,7 +106,6 @@ public final class LocalizationManager
             UIManager.put("ColorChooser.okText", "ОК");
             UIManager.put("ColorChooser.cancelText", "Отмена");
             UIManager.put("ColorChooser.previewText", "Предпросмотр");
-
         }
         catch (Exception e)
         {
